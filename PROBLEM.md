7366c6ec8430a8d70fa05aa040ebb625a02d4283
Henry Wang <theKidOfArcrania@users.noreply.github.com>
2024-04-21 04:12:14 -0700

    Add problem meta stuff

---
+++ PROBLEM.md
@@ -0,0 +1,12 @@
+# TTYD: The Cryptic Letter
+
+> Me: Mom can we get the remake for TTYD?
+> Mom: We have a remake of TTYD at home.
+> Remake at home:
+
+Use the TTYD US iso file. Command to patch binary:
+```sh
+./mpatch -s TTYD_US.iso -p mod.xdelta -o output_mod.iso
+```
+
+
--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 echo "[#] Building mod..."
 ./ttyd-mod-manager -r . build
-echo "[#] Converting to iso..."
-dolphin-tool convert -i patch/P-G8ME/sys/main.dol -o main.iso -f iso
 echo "[#] Generating delta..."
-xdelta3 -f -e -s ./TTYD_US.iso main.iso mod.xdelta
+./mpatch -s TTYD_US.iso -i patch/P-G8ME -p mod.xdelta
+echo "[#] Creating handout..."
+cp mod.xdelta mpatch handout/
+tar -acvf handout.tar.gz handout/
